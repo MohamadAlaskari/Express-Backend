bed62c8e4323a60fc61c70835472b313c8a1f581
MohamadAlaskari <m_alskari96@hotmail.com>
2024-01-17 17:35:05 +0100

    install env-cmd

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ app.use(require('./middleware/errorHandler'));
 
 //dbMongo.connect();
 //dbMySQL.connect();
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}/ 🔥`);
