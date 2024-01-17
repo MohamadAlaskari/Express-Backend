58e709bd611bda367350f7ff84369b6d3bec1e46
MohamadAlaskari <m_alskari96@hotmail.com>
2024-01-17 15:32:07 +0100

    fix bugs

--- server/controllers/userControler.js
+++ server/controllers/userControler.js
@@ -1,43 +1,39 @@
-const userModel = require('./../models/mysql/userModel')
+const userModel = require('./../models/mysql/userModel');
 
-getUser = (req, res, next) => {
+const getUser = (req, res, next) => {
     try {
-
         // Assuming getting is successful
-        res.status(200).res("users");// 200 ok
+        res.status(200).send("users"); // 200 OK
     } catch (error) {
         next(error); // Forward error to error handling middleware
     }
-}
+};
 
-createUser = (req, res, next) => {
+const createUser = (req, res, next) => {
     try {
-
         // Assuming creating is successful
-        res.status(201).res("user Created");// 201 ok
+        res.status(201).send("user Created"); // 201 Created
     } catch (error) {
         next(error); // Forward error to error handling middleware
     }
-}
+};
 
-updateUser = (req, res, next) => {
+const updateUser = (req, res, next) => {
     try {
-
         // Assuming update is successful
-        res.status(200).res("user updated");// 200 ok
+        res.status(200).send("user updated"); // 200 OK
     } catch (error) {
         next(error); // Forward error to error handling middleware
     }
-}
+};
 
-deleteUser = (req, res, next) => {
+const deleteUser = (req, res, next) => {
     try {
-
         // Assuming delete is successful
-        res.status(204).res();// 200 ok
+        res.status(204).send(); // 204 No Content
     } catch (error) {
         next(error); // Forward error to error handling middleware
     }
-}
+};
 
-module.exports = { getUser, createUser, updateUser, deleteUser }
+module.exports = { getUser, createUser, updateUser, deleteUser };
--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,4 +1,6 @@
 module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Ein interner Serverfehler ist aufgetreten.');
+    
+    const status = err.status || 500;
+    const message = err.message || 'An internal server error has occurred.';
+    res.status(status).send(message);
 };
