52f6e83aabc43d88d090e0298a146dedd76394b3
MohamadAlaskari <m_alskari96@hotmail.com>
2024-01-17 14:04:26 +0100

    create strucktur

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+//const dbMongo = require('./config/dbMongo');
+//const dbMySQL = require('./config/dbMySQL');
+
+app.use(express.json());
+app.use('/api', routes);
+
+// Zentrale Fehlerbehandlung
+app.use(require('./middleware/errorHandler'));
+
+//dbMongo.connect();
+//dbMySQL.connect();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/ 🔥`);
+});
+
+module.exports = app;
